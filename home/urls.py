8d51b581852aeffd0acefe82db200eef3635a2a0
junior007x9 <santos.junior12@hotmail.com>
2024-12-02 19:48:52 -0300

    att

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categorias/', views.categoria, name='categoria'),  # Lista de categorias
     path('produtos/', views.lista_produtos, name='lista_produtos'),  # Lista de produtos
+    path('form_categoria/', views.form_categoria, name= 'form_categoria'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Categoria
+from .models import *
+from .forms import *
+from django.shortcuts import render, redirect
+
 
 def index(request):
     return render(request, 'index.html')
@@ -15,3 +19,17 @@ def lista_produtos(request):
     # Exibe a lista de produtos
     lista = Categoria.objects.all()  
     return render(request, 'categoria/lista.html', {'lista': lista})
+
+
+def form_categoria(request):
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            form.save() # salva a instancia do modelo no banco de dados
+            return redirect('categoria') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = CategoriaForm() # formulário vazio
+    contexto = {
+        'form':form,
+    }
+    return render(request, 'categoria/formulario.html', contexto)
