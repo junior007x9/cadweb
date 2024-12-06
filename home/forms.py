956f6a83664a4f52a7b50a9f4123e28efe80d007
junior007x9 <santos.junior12@hotmail.com>
2024-12-06 12:16:52 -0300

    att

---
+++ home/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import *
+from django.db import models
+
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nome', 'ordem']
+        widgets = {
+            'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
+            'ordem':forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
+        }
+        
