bccc77a8f7df99b7c5deba5a48305e66ca9193a3
junior007x9 <santos.junior12@hotmail.com>
2024-12-14 11:40:31 -0300

    att

--- home/models.py
+++ home/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     ordem = models.IntegerField(unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)  # Adicionado campo de data de criação
+    created_at = models.DateTimeField(default=timezone.now)  # Valor padrão temporário
 
     def __str__(self):
         return self.nome
 
     class Meta:
-        ordering = ['-created_at']  # Ordenação padrão por data de criação (mais recente primeiro)
+        ordering = ['-created_at']
